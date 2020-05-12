307d123ba77ab45134b002fa4e3a83c89cd04d3e
Miguel Salinas <miguel.salinas@usach.cl>
2020-05-11 20:37:32 -0400

    Función generadora de ID

--- github_20215515_SalinasGonzález.rkt
+++ github_20215515_SalinasGonzález.rkt
@@ -1,9 +1,28 @@
 #lang racket
+;Se define función git que recibirá los comandos
+(define git (lambda (x) (cond ;se identifica y se ejecuta la función identificada
+                           ((equal? x "pull") pull)
+                           ((equal? x "add") add)
+                           ((equal? x "commit") commit)
+                           ((equal? x "push") push))))
+
+;Se define la función add
+(define add (lambda (nombreArchivo) (nombreArchivo) ))
+;Se define la función pull
+(define pull (lambda (nombreArchivo) (nombreArchivo) ))
+;Se define la función commit
+(define commit (lambda (nombreArchivo) (nombreArchivo) ))
+;Se define la función push
+(define push (lambda (nombreArchivo) (nombreArchivo) ))
+
+;Se define generador de ID
+(define generadorID (lambda (nombreArchivo)(list nombreArchivo (random 100))))
+
 ;TDA Zonas : Es una lista de TDA's donde cada uno de los argumentos representa cada una de las zonas donde se traspasa la información
-(define zonas (lambda (workSpace index localRepository remoteRepository) (if(and(list? workSpace);
-                                                                                (list? index);
-                                                                                (list? localRepository); TDA Local Repository : Una lista que contiene id - fecha y hora - nombre de realizador - comentario - listado de archivos del commit - id version anterior
-                                                                                (list? remoteRepository);
+(define zonas (lambda (workSpace index localRepository remoteRepository) (if(and(list? workSpace);TDA workSpace:  Consta de una lista que contiene los nombres de los archivos presentes en el directorio local del PC y sus id respectivas
+                                                                                (list? index); TDA index: Consta de una lista
+                                                                                (list? localRepository); TDA Local Repository : Consta de una lista que contiene id - fecha y hora - nombre de realizador - comentario - listado de archivos del commit - id version anterior
+                                                                                (list? remoteRepository);TDA workSpace :
                                                                                 )(list workSpace index localRepository remoteRepository);retorno si es que todos son listas
                                                                                  null);retorno si uno de los argumentos no es lista
                                                                                 ))
