413fc111aa3e7d2a27d9d292a757c12497276fc8
Miguel Salinas <miguel.salinas@usach.cl>
2020-05-25 21:34:29 -0400

    modificaciones tda

--- github_20215515_SalinasGonzález.rkt
+++ github_20215515_SalinasGonzález.rkt
@@ -15,14 +15,29 @@
 ;Se define la función push
 (define push (lambda (nombreArchivo) (nombreArchivo) ))
 
+(define workSpace (lambda (x) (nombreArchivo) ))
+(define index (lambda (nombreArchivo) (nombreArchivo) ))
+(define localRepository (lambda (nombreArchivo) (nombreArchivo) ))
+(define remoteRepository (lambda (nombreArchivo) (nombreArchivo) ))
+
 ;Se define generador de ID
 (define generadorID (lambda (nombreArchivo)(list nombreArchivo (random 100))))
 
+(require racket/date)
+(define fecha (lambda () (date->string (current-date) second)))
+
+(define obtenerDatos (lambda (datos) (if(string? datos) (list datos (fecha)) null)))
+
+(define datos? (lambda x (if(list? x) (if(and (string?(car(cdr x))) (string?(car x) (null?(cdr (cdr x))))) #t #f) #f )     ))
+
+
 ;TDA Zonas : Es una lista de TDA's donde cada uno de los argumentos representa cada una de las zonas donde se traspasa la información
-(define zonas (lambda (workSpace index localRepository remoteRepository) (if(and(list? workSpace);TDA workSpace:  Consta de una lista que contiene los nombres de los archivos presentes en el directorio local del PC y sus id respectivas
+(define zonas (lambda (workSpace index localRepository remoteRepository datos comando) (if(and(list? workSpace);TDA workSpace:  Consta de una lista que contiene los nombres de los archivos presentes en el directorio local del PC y sus id respectivas
                                                                                 (list? index); TDA index: Consta de una lista
                                                                                 (list? localRepository); TDA Local Repository : Consta de una lista que contiene id - fecha y hora - nombre de realizador - comentario - listado de archivos del commit - id version anterior
                                                                                 (list? remoteRepository);TDA workSpace :
-                                                                                )(list workSpace index localRepository remoteRepository);retorno si es que todos son listas
+                                                                                (datos? datos)
+                                                                                (string? comando)
+                                                                                )(list workSpace index localRepository remoteRepository datos comando);retorno si es que todos son listas
                                                                                  null);retorno si uno de los argumentos no es lista
                                                                                 ))
