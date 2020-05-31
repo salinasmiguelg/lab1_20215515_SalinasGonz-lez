77178e717528e884481e3f79d5a6ac003ebd0311
Miguel Salinas <miguel.salinas@usach.cl>
2020-05-31 02:16:29 -0400

    zonas->string listo

--- github_20215515_SalinasGonzález.rkt
+++ github_20215515_SalinasGonzález.rkt
@@ -1,10 +1,10 @@
 #lang racket
 ;Se define función git que recibirá los comandos
-(define git (lambda (x) (cond ;se identifica y se ejecuta la función identificada
-                           ((equal? x "pull") pull)
-                           ((equal? x "add") add)
-                           ((equal? x "commit") commit)
-                           ((equal? x "push") push))))
+(define git (lambda (x)(lambda (zona) (lambda (acompanamiento)(cond ;se identifica y se ejecuta la función identificada
+                           ((equal? x "pull") (pull zona))
+                           ((equal? x "add") ((add zona)acompanamiento))
+                           ((equal? x "commit") ((commit zona)acompanamiento))
+                           ((equal? x "push") (push zona)))))))
 
 
 (define append (lambda (listOne listTwo)
@@ -14,7 +14,7 @@
       )
   ))
 
-(define z1 (list (list "n1" "n2" "n3") null null null "repo Miguel"))
+
 
 
 (define comprobarExistencia (lambda (x lista) (if(null? lista) #f (if (equal? x (car lista)) #t (comprobarExistencia x (cdr lista))))))
@@ -25,16 +25,15 @@
 (define generadorCommit (lambda (comentario) (lambda (nombre) (list nombre comentario))))
 
 (define quitarCommit (lambda (lista) (list-ref lista 0)) )
-(define mapeoQuitar (lambda (lista) (map quitarCommit lista)))
 ;Se define la función add
-(define add (lambda (zona) (lambda (archivos) (if(list? archivos) (if(comprobarAdd (list-ref zona 0) archivos) (list (list-ref zona 0) (append (list-ref zona 1) archivos) (list-ref zona 2) (list-ref zona 3) (list-ref zona 4)  ) zona) zona) )))
+(define add (lambda (zona) (lambda (archivos) (if(list? archivos) (if(comprobarAdd (list-ref zona 0) archivos) (list (list-ref zona 0) (append (list-ref zona 1) archivos) (list-ref zona 2) (list-ref zona 3) (list-ref zona 4) (string-append (list-ref zona 5) "(" (string-append "add " (date) ) ")" " ") ) zona) zona) )))
 
 ;Se define la función commit
-(define commit (lambda (zona) (lambda (comentario) (if (string? comentario) (list (list-ref zona 0) null (map (generadorCommit comentario) (list-ref zona 1)) (list-ref zona 3) (list-ref zona 4) )  zona) )))
+(define commit (lambda (zona) (lambda (comentario) (if (string? comentario) (list (list-ref zona 0) null (map (generadorCommit comentario) (list-ref zona 1)) (list-ref zona 3) (list-ref zona 4) (string-append (list-ref zona 5) "(" (string-append "commit " (date) ) ")" " ") )  zona) )))
 ;Se define la función push
-(define push (lambda (zona) (list (list-ref zona 0) (list-ref zona 1) (list-ref zona 2) (append (list-ref zona 3) (list-ref zona 2)) (list-ref zona 4) ) ))
+(define push (lambda (zona) (list (list-ref zona 0) (list-ref zona 1) (list-ref zona 2) (append (list-ref zona 2) (list-ref zona 3)) (list-ref zona 4) (string-append (list-ref zona 5) "(" (string-append "push " (date) ) ")" " ") ) ))
 ;Se define la función pull
-(define pull (lambda (zona) (list (append (list-ref zona 0) (mapeoQuitar (list-ref zona 3 ))) (list-ref zona 1) (list-ref zona 2) (list-ref zona 3) (list-ref zona 4) ) ))
+(define pull (lambda (zona) (list (append (list-ref zona 0) (map quitarCommit (list-ref zona 3 ))) (list-ref zona 1) (list-ref zona 2) (list-ref zona 3) (list-ref zona 4) (string-append (list-ref zona 5) "(" (string-append "pull " (date) ) ")" " ") ) ))
 
 
 ;El TDA de workSpace se define como una lista de strings, de cualquier largo. Por lo que no se presenta constructor de este TDA.
@@ -48,14 +47,17 @@
 (define generadorID (lambda (nombreArchivo)(list nombreArchivo (random 100))))
 
 (require racket/date)
-(define fecha (lambda () (date->string (current-date) second)))
+(define date (lambda () (date->string (current-date) second)))
 
-(define obtenerDatos (lambda (datos) (if(string? datos) (list datos (fecha)) null)))
+(define obtenerDatos (lambda (datos) (if(string? datos) (list datos (date)) null)))
 
 (define datos? (lambda x (if(list? x) (if(and (string?(car(cdr x))) (string?(car x) (null?(cdr (cdr x))))) #t #f) #f )     ))
 
 
-;(define extraerWorkSpace (lambda (zona)  ))
+(define extraer (lambda (lista aux) (if (null? lista) "\n" (if (null? (cdr lista)) (string-append aux (car lista) "\n") (extraer (cdr lista) (string-append aux (car lista) " ")) ) )))
+(define extraerListaDeLista (lambda (lista aux) (if (null? lista) "\n" (if (null? (cdr lista)) (string-append aux "(" (list-ref (car lista) 0) " " (list-ref (car lista) 1) ")" "\n" ) (extraerListaDeLista (cdr lista) (string-append aux "(" (list-ref (car lista) 0) " " (list-ref (car lista) 1) ")" " ")) ) )))
+
+(define zonas->string (lambda (zona) (string-append "Nombre del repositorio: " (car(list-ref zona 4)) " "  (car(cdr(list-ref zona 4))) "\nWorkSpace: " (extraer (list-ref zona 0) "") "Index: " (extraer (list-ref zona 1) "") "LocalRepository: " (extraerListaDeLista (list-ref zona 2) "") "RemoteRepository: " (extraerListaDeLista (list-ref zona 3) "") "Historial de comandos: " (list-ref zona 5) )))
 
 
 ;TDA Zonas : Es una lista de TDA's donde cada uno de los argumentos representa cada una de las zonas donde se traspasa la información
@@ -64,7 +66,11 @@
                                                                                 (list? localRepository); TDA Local Repository : Consta de una lista que contiene id - fecha y hora - nombre de realizador - comentario - listado de archivos del commit - id version anterior
                                                                                 (list? remoteRepository);TDA workSpace :
                                                                                 (datos? datos)
-                                                                                (string? comando)
+                                                                                (list? comando)
                                                                                 )(list workSpace index localRepository remoteRepository datos comando);retorno si es que todos son listas
                                                                                  null);retorno si uno de los argumentos no es lista
                                                                                 ))
+
+
+(define z1 (list (list "n1" "n2" "n3") null null null (obtenerDatos "repo Miguel") ""))
+;(push((commit((add z1)(list "n1" "n2" "n3")) )"skere"))
